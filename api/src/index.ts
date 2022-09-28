4cdf3850fff541a2e65c0d06a092d229c440c33e
inakielha <inakielhaiek@gmail.com>
2022-09-27 21:25:15 -0300

    front y socketio

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,22 @@
 import express from "express"
 const cors = require('cors')
 const {dbConnection} = require("./dataBase/db")
+const path = require("path")
+const http = require("http")
+const {Server} = require("socket.io");
 require('dotenv').config()
+
 
 const app = express()
 dbConnection()
+
 
 app.set("port", process.env.PORT || 2500)
 
 app.use(cors())
 
+// Static files 
+// app.use(express.static(path.join(__dirname, "public")))
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
@@ -21,6 +28,18 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.listen(app.get("port"),()=>{
+const server = app.listen(app.get("port"),()=>{
     console.log("server is on port" + " " + process.env.PORT)
+})
+
+const serverSocket = http.createServer(app)
+
+const io = new Server(server,{
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET","POST","DELETE"],
+    },
+})
+io.on("connection",(socket:any)=>{
+    console.log(`new connection: ${socket.id}`)
 })
