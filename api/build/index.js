d7f0662d05cb36763f8a1a625e6902f9f25af5bd
PowChorba <101152834+PowChorba@users.noreply.github.com>
2022-12-07 11:08:10 -0300

    deploy

--- api/build/index.js
+++ api/build/index.js
@@ -58,19 +58,19 @@ io.on('connect', (socket) => {
         addUser(userId, socket.id);
         io.emit('getUsers', users);
     });
-    // socket.on('disconnect', async() => {
-    //     let user = getUserBySocket(socket.id)
-    //     removeUser(user?.userId||"")
-    //     console.log('a user disconnected', user?.userId)
-    //     io.emit('getUsers', users)
-    //     try {
-    //         const res = await axios.put("https://fastchat-production.up.railway.app/disconnect", user)
-    //         console.log(res.data)
-    //         io.emit("userDisconnected", {userId:user?.userId, data:(res.data.ok? res.data : "")})
-    //     }catch (e){
-    //         console.log(e)
-    //     }
-    // })
+    socket.on('disconnect', async() => {
+        let user = getUserBySocket(socket.id)
+        removeUser(user?.userId||"")
+        console.log('a user disconnected', user?.userId)
+        io.emit('getUsers', users)
+        try {
+            const res = await axios.put("https://fastchat-production.up.railway.app/disconnect", user)
+            console.log(res.data)
+            io.emit("userDisconnected", {userId:user?.userId, data:(res.data.ok? res.data : "")})
+        }catch (e){
+            console.log(e)
+        }
+    })
     socket.on('sendMessage', ({ senderId, receiverId, text, senderChat, messageId, isGroup, isImage, isAudio }) => {
         if (!isGroup) {
             const user = getUser(receiverId);
