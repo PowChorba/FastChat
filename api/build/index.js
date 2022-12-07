7e342240b7e82e5790207f8be0629506efeea788
PowChorba <101152834+PowChorba@users.noreply.github.com>
2022-12-07 10:39:15 -0300

    deploy

--- api/build/index.js
+++ api/build/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -17,7 +8,6 @@ const cors = require('cors');
 const { dbConnection } = require("./dataBase/db");
 const index_1 = __importDefault(require("./routes/index"));
 const socket_io_1 = require("socket.io");
-const axios_1 = __importDefault(require("axios"));
 require('dotenv').config();
 const app = (0, express_1.default)();
 dbConnection();
@@ -68,20 +58,19 @@ io.on('connect', (socket) => {
         addUser(userId, socket.id);
         io.emit('getUsers', users);
     });
-    socket.on('disconnect', () => __awaiter(void 0, void 0, void 0, function* () {
-        let user = getUserBySocket(socket.id);
-        removeUser((user === null || user === void 0 ? void 0 : user.userId) || "");
-        console.log('a user disconnected', user === null || user === void 0 ? void 0 : user.userId);
-        io.emit('getUsers', users);
-        try {
-            const res = yield axios_1.default.put("https://fastchat-production.up.railway.app/disconnect", user);
-            console.log(res.data);
-            io.emit("userDisconnected", { userId: user === null || user === void 0 ? void 0 : user.userId, data: (res.data.ok ? res.data : "") });
-        }
-        catch (e) {
-            console.log(e);
-        }
-    }));
+    // socket.on('disconnect', async() => {
+    //     let user = getUserBySocket(socket.id)
+    //     removeUser(user?.userId||"")
+    //     console.log('a user disconnected', user?.userId)
+    //     io.emit('getUsers', users)
+    //     try {
+    //         const res = await axios.put("https://fastchat-production.up.railway.app/disconnect", user)
+    //         console.log(res.data)
+    //         io.emit("userDisconnected", {userId:user?.userId, data:(res.data.ok? res.data : "")})
+    //     }catch (e){
+    //         console.log(e)
+    //     }
+    // })
     socket.on('sendMessage', ({ senderId, receiverId, text, senderChat, messageId, isGroup, isImage, isAudio }) => {
         if (!isGroup) {
             const user = getUser(receiverId);
