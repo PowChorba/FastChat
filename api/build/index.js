145bee877c2d669cd9d6165240c1aad62276cc9a
PowChorba <101152834+PowChorba@users.noreply.github.com>
2022-12-07 11:19:36 -0300

    deploy

--- api/build/index.js
+++ api/build/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -38,9 +29,9 @@ const server = app.listen(app.get("port"), () => {
     console.log("Server is on port" + " " + process.env.PORT);
 });
 const io = new socket_io_1.Server(server, {
-// cors: {
-//     origin: ['http://127.0.0.1:5641', 'http://localhost:3000']
-// }
+    cors: {
+    // origin: ['http://127.0.0.1:5641','http://localhost:3000']
+    }
 });
 let users = [];
 let groups = [];
